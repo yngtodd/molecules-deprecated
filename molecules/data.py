4ada81e6261d2fdef860086a009546ea82fb0af8
Todd <myoung44@vols.utk.edu>
2018-01-17 22:24:02 -0500

    :pencil: :zap:

--- molecules/data.py
+++ molecules/data.py
@@ -12,12 +12,12 @@ class FSPeptide(Dataset):
         """
         Parameters:
         ----------
-            * `data` (string, [Numpy format]):
-                Path to the data.
-            * `label` (string, [Numpy format]):
-                Path to the labels.
-            * `transform` (callable, optional):
-                Optional transform to be applied on a sample.
+        data : str
+            Path to the data in numpy format.
+        label : str
+            Path to the labels.
+        transform : callable, optional
+            Optional transform to be applied on a sample.
         """
         self.data = np.load(data)
         self.labels = np.load(labels)
