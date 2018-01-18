8091283ed6ca8475d8dd1f808f04869284440689
Todd <myoung44@vols.utk.edu>
2018-01-18 13:44:25 -0500

    touching up decoder

--- molecules/decoder.py
+++ molecules/decoder.py
@@ -6,11 +6,19 @@ import torch.nn.functional as F
 class Decoder(nn.Module):
     def __init__(self, latent_size, output_size, kernel1=4, stride1=2, kernel2=4,
                  stride2=2, kernel3=4, stride3=2, kernel4=4, stride4=2,
-                 kernel5=4, stride5=2)
+                 kernel5=4, stride5=2):
         super(Decoder, self).__init__()
         """
         Parameters:
         ----------
+        latent_size : int
+            latent dimension size of the autoencoder.
+        output_size : int
+            Output dimension for the data. Should equal input_dimension of AE.
+        kernel* : int, defualt=4
+            Convolutional filter size for layer *.
+        stride* : int, default=2
+            Stride length for convolutional filter at layer *.
         """
         self.latent_size = latent_size
         self.output_size = output_size
@@ -48,3 +56,19 @@ class Decoder(nn.Module):
 
             nn.Linear(128, self.outputsize)
         )
+
+
+        def forward(self, latent_input):
+            """
+            Parameters:
+            ----------
+            input : float tensor shape=(batch_size, input_size)
+            Returns:
+            -------
+            A float tensor with shape (batch_size, latent_variable_size)
+            """
+            # Transpose input to the shape of [batch_size, embed_size, seq_len]
+            input = torch.transpose(latent_input, 1, 2)
+
+            result = self.cnn(input)
+            return result.squeeze(2)
