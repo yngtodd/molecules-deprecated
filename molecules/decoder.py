4cca98210e8c2e63f1adb040fd23a3c9a30d25fc
Todd <myoung44@vols.utk.edu>
2018-01-22 12:25:17 -0500

    Fixing decoder tensor shapes

--- molecules/decoder.py
+++ molecules/decoder.py
@@ -30,36 +30,34 @@ class Decoder(nn.Module):
         self.stride3 = stride3
         self.kernel4 = kernel4
         self.stride4 = stride4
-        self.kernel5 = kernel5
-        self.stride5 = stride5
 
         self.fc = nn.Linear(self.latent_size, 256)
 
         self.cnn_decoder1 = nn.Sequential(
-            nn.ConvTranspose2d(1, 32, self.kernel1, self.stride1, padding=2),
+            nn.ConvTranspose2d(1, 16, self.kernel1, self.stride1, padding=2),
             nn.MaxPool2d(2),
             nn.ELU()
         )
 
         self.cnn_decoder2 = nn.Sequential(
-            nn.ConvTranspose2d(32, 64, self.kernel2, self.stride2),
+            nn.ConvTranspose2d(16, 32, self.kernel2, self.stride2),
             nn.MaxPool2d(2),
             nn.ELU()
         )
 
         self.cnn_decoder3 = nn.Sequential(
-            nn.ConvTranspose2d(64, 64, self.kernel3, self.stride3),
+            nn.ConvTranspose2d(32, 64, self.kernel3, self.stride3),
             nn.MaxPool2d(2),
             nn.ELU()
         )
 
         self.cnn_decoder4 = nn.Sequential(
-            nn.ConvTranspose2d(64, 128, self.kernel4, self.stride4),
+            nn.ConvTranspose2d(64, 64, self.kernel4, self.stride4),
             nn.MaxPool2d(2),
             nn.ELU()
         )
 
-        #self.fc2 = nn.Linear(128, self.output_size)
+        self.fc2 = nn.Linear(20736, self.output_size)
 
     def forward(self, latent_input):
         """
@@ -73,18 +71,15 @@ class Decoder(nn.Module):
         """
         print("latent input in decoder {}".format(latent_input))
         out = self.fc(latent_input)
-        #out = torch.transpose(latent_input, 4, 4)
-        #out = out.view(out.size(0), 1, 16, 16)
-        #print('output of decoder fc1 has shape {}'.format(out.shape))
-        #latent_input = latent_input.unsqueeze(0)
-        #out = latent_input.view(latent_input.size(0), 1, 4, 4)
-        #print("unsqueezed latent input has shape {}".format(latent_input.shape))
-        out = out.view(out.size(0), 1, 16, 16)
         print('output of linear layer has shape {}'.format(out.shape))
+        out = out.view(out.size(0), 1, 16, 16)
+        print('reshaped output of linear layer has shape {}'.format(out.shape))
         out = self.cnn_decoder1(out)
         print('output of decoder1 cnn has shape {}'.format(out.shape))
-        #out = out.view(out.size(0), -1)
-        #print('input into fully connected layer of decoder has shape {}'.format(out.shape))
-        #out = self.fc2(out)
-        #print('output of decoder has shape {}\n'.format(out.shape))
+        out = self.cnn_decoder2(out)
+        out = self.cnn_decoder3(out)
+        out = self.cnn_decoder4(out)
+        print('output of cnn_decoder 4 has shape {}'.format(out))
+        out = out.view(out.size(0), -1)
+        out = self.fc2(out)
         return out
