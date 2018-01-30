97177fc91108068d1a1bae25a20882480b614ca6
yngtodd <young.todd.mk@gmail.com>
2018-01-30 14:55:28 -0500

    Wrapping up BCE and KL loss functions

--- molecules/variational_autoencoder/main.py
+++ molecules/variational_autoencoder/main.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.nn import functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
@@ -31,13 +32,26 @@ parser.add_argument('--save_path', type=str, default='./save_points/kl_bce_laten
                     help='Path to where to save the model weights.')
 args = parser.parse_args()
 
+def loss_function(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 441))
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # https://arxiv.org/abs/1312.6114
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    # Normalise by same number of elements as in reconstruction
+    KLD /= args.batch_size * 441 
+
+    return BCE + KLD
+
 
 def main():
     use_cuda = args.use_cuda
     print("Cuda set to {} | Cuda availability: {}".format(use_cuda, torch.cuda.is_available()))
 
     experiment = "vae_latent3"
-    logger = SummaryWriter(log_dir='./logs', comment=experiment)
+    #logger = SummaryWriter(log_dir='./logs', comment=experiment)
 
     train_data = UnlabeledContact(data='/home/ygx/data/fspeptide/fs_peptide.npy')
     print('Number of samples: {}'.format(len(train_data)))
@@ -49,13 +63,13 @@ def main():
     encoder = Encoder(input_size=input_size, latent_size=3)
     decoder = Decoder(latent_size=3, output_size=input_size)
     vae = VAE(encoder, decoder, use_cuda=use_cuda)
-    criterion = nn.BCELoss()
+    #criterion = nn.BCELoss()
 
     if use_cuda:
-        encoder = encoder.cuda()
-        decoder = decoder.cuda()
-        vae = vae.cuda()
-        criterion = criterion.cuda()
+        encoder = encoder.cuda().half()
+        decoder = decoder.cuda().half()
+        vae = vae.cuda().half()
+        #criterion = criterion.cuda().half()
 
     optimizer = optim.SGD(vae.parameters(), lr = 0.01)
 
@@ -68,7 +82,7 @@ def main():
             inputs = inputs.resize_(args.batch_size, 1, 21, 21)
             inputs = inputs.float()
             if use_cuda:
-                inputs = inputs.cuda()
+                inputs = inputs.cuda().half()
             inputs = Variable(inputs)
 
             # Compute output
@@ -76,8 +90,9 @@ def main():
             dec = vae(inputs)
 
             # Measure the loss
-            kl = kl_loss(vae.z_mean, vae.z_sigma)
-            loss = criterion(dec, inputs) #+ kl # Adding KL is caussing loss > 1
+            #kl = kl_loss(vae.z_mean, vae.z_sigma)
+            #loss = criterion(dec, inputs) #+ kl # Adding KL is caussing loss > 1
+            loss = loss_function(dec, inputs, vae.z_mean, vae.z_sigma)
             losses.update(loss.data[0], inputs.size(0))
 
             # Compute the gradient
@@ -90,46 +105,46 @@ def main():
             #logger.add_graph_onnx(vae)
 
             # log loss values every iteration
-            logger.add_scalar('data/(train)loss_val', losses.val, batch_idx + 1)
-            logger.add_scalar('data/(train)loss_avg', losses.avg, batch_idx + 1)
+            #logger.add_scalar('data/(train)loss_val', losses.val, batch_idx + 1)
+            #logger.add_scalar('data/(train)loss_avg', losses.avg, batch_idx + 1)
 
             # log the layers and layers gradient histogram and distributions
-            for tag, value in vae.named_parameters():
-                tag = tag.replace('.', '/')
-                logger.add_histogram('model/(train)' + tag, to_numpy(value), batch_idx + 1)
-                logger.add_histogram('model/(train)' + tag + '/grad', to_numpy(value.grad), batch_idx + 1)
+            #for tag, value in vae.named_parameters():
+            #    tag = tag.replace('.', '/')
+            #    logger.add_histogram('model/(train)' + tag, to_numpy(value), batch_idx + 1)
+            #    logger.add_histogram('model/(train)' + tag + '/grad', to_numpy(value.grad), batch_idx + 1)
 
             # log the outputs of the autoencoder 
-            logger.add_image('model/(train)output', make_grid(dec.data), batch_idx + 1)
+            #logger.add_image('model/(train)output', make_grid(dec.data), batch_idx + 1)
 
             if batch_idx % args.log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(data), len(trainloader.dataset),
                     100. * batch_idx / len(trainloader), loss.data[0]))
        
-        if epoch < 10:
+        #if epoch < 10:
             # Get latent encoding
-            latent_array = encoder(inputs).data[0].cpu().numpy()
-            filename = 'latent_epoch' + str(epoch)
-            np.save('./latent_saves/kl_bce_latent3/' + filename, latent_array)
+            #latent_array = encoder(inputs).data[0].cpu().numpy()
+            #filename = 'latent_epoch' + str(epoch)
+            #np.save('./latent_saves/kl_bce_latent3/' + filename, latent_array)
             
             # Get reconstructed image
-            reconstructed_array = vae(inputs).data[0].cpu().numpy().reshape(21, 21)
-            recon_filename = 'reconstructed_epoch' + str(epoch)
-            np.save('./reconstruct_saves/kl_bce_latent3/' + recon_filename, reconstructed_array)
+            #reconstructed_array = vae(inputs).data[0].cpu().numpy().reshape(21, 21)
+            #recon_filename = 'reconstructed_epoch' + str(epoch)
+            #np.save('./reconstruct_saves/kl_bce_latent3/' + recon_filename, reconstructed_array)
         
         if epoch % 10 == 0:
             torch.save(vae.state_dict(), args.save_path + 'epoch' + str(epoch))
 
-            latent_array = encoder(inputs).data[0].cpu().numpy()
-            filename = 'latent_epoch' + str(epoch)
-            np.save('./latent_saves/kl_bce_latent3/' + filename, latent_array)
+            #latent_array = encoder(inputs).data[0].cpu().numpy()
+            #filename = 'latent_epoch' + str(epoch)
+            #np.save('./latent_saves/kl_bce_latent3/' + filename, latent_array)
 
-            reconstructed_array = vae(inputs).data[0].cpu().numpy().reshape(21, 21)
+            reconstructed_array = vae(inputs).data[0].cpu().float().numpy().reshape(21, 21)
             recon_filename = 'reconstructed_epoch' + str(epoch)
             np.save('./reconstruct_saves/kl_bce_latent3/' + recon_filename, reconstructed_array)
 
-        logger.close()
+        #logger.close()
 
 
 if __name__ == '__main__':
