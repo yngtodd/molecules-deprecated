b01d9374f4d4547260cc8f8002c71ee38ce1073e
Todd <myoung44@vols.utk.edu>
2018-02-01 00:04:12 -0500

    Adding metrics module for various loss functions

---
+++ molecules/variational_autoencoder/metrics.py
@@ -0,0 +1,38 @@
+import torch
+from torch.nn import functional as F
+
+
+def entropy_kl_loss(recon_x, img, mu, logvar):
+    """
+    Loss function: binary cross entropy plus KL divergence.
+
+    Parameters:
+    ----------
+    recons_x : Pytorch tensor
+        Reconstructed tensor from VAE.
+
+    img : Pytorch tensor
+        True image the VAE is trying to reconstruct.
+
+    mu : Pytorch tensor
+        Mean tensor from VAE
+
+    logvar : Pytorch tensor
+        Log variance from VAE
+
+    Returns:
+    -------
+    Binary cross entropy + KL divergence loss : float
+
+    References:
+    ----------
+    Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    Appendix B
+    https://arxiv.org/abs/1312.6114
+    """
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 441))
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    # Normalise by same number of elements as in reconstruction
+    KLD /= args.batch_size * 441
+    return BCE + KLD
