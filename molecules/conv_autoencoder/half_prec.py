9b096e7e1ea10d37f52151eabb03f9304f581058
Michael T. Young <ygx@benson.ornl.gov>
2018-05-30 22:24:16 -0400

    Sorting out convolutions

--- molecules/conv_autoencoder/half_prec.py
+++ molecules/conv_autoencoder/half_prec.py
@@ -4,13 +4,10 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
-from encoder import Encoder
-from decoder import Decoder
-from fc_decoder import FCDecoder
-from vae import VAE
-from vae import latent_loss
 from data import FSPeptide
 from data import UnlabeledContact
+
+from model import AutoEncoder
 
 import numpy as np
 import argparse
@@ -40,20 +37,16 @@ def main():
 
     # Contact matrices are 21x21
     input_size = 441
+    img_height = 21
+    img_width = 21
 
-    encoder = Encoder(input_size=input_size, latent_size=3)
-    decoder = Decoder(latent_size=3, output_size=input_size)
-    vae = VAE(encoder, decoder, use_cuda=use_cuda)
-    criterion = nn.MSELoss()
+    vae = AutoEncoder(code_size=20, imgsize=input_size, height=img_height, width=img_width)
+    criterion = nn.BCELoss()
 
     if use_cuda:
-        encoder = nn.DataParallel(encoder)
-        decoder = nn.DataParallel(decoder)
-        encoder = encoder.cuda().half()
-        decoder = decoder.cuda().half()
         vae = nn.DataParallel(vae)
         vae = vae.cuda().half()
-        criterion = criterion.cuda().half()
+        criterion = criterion.cuda()
 
     optimizer = optim.SGD(vae.parameters(), lr = 0.01)
 
@@ -64,15 +57,14 @@ def main():
     for epoch in range(15):
         for batch_idx, data in enumerate(trainloader):
             inputs = data['cont_matrix']
- #           inputs = inputs.resize_(args.batch_size, 1, 21, 21)
+            inputs = inputs.resize_(args.batch_size, 1, 21, 21)
             inputs = inputs.float()
             if use_cuda:
                 inputs = inputs.cuda().half()
             inputs = Variable(inputs)
             optimizer.zero_grad()
-            dec = vae(inputs)
-            ll = latent_loss(vae.z_mean, vae.z_sigma)
-            loss = criterion(dec, inputs) + ll
+            output, code = vae(inputs)
+            loss = criterion(output, inputs)
             loss.backward()
             optimizer.step()
             epoch_loss += loss.data[0]
