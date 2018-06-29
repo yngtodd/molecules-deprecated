9819d432be2f4aee58a88187b22bb51688196481
braceal <braceal@umich.edu>
2018-06-29 09:58:08 -0400

    OOP changes to AutoEncoder.py, added encoder and decoder objects

--- Molecules/AutoEncoder.py
+++ Molecules/AutoEncoder.py
@@ -1,12 +1,82 @@
 # import all autoencoder versions
 
+class ConvEncoder(object):
+    def __init__(self, type, latent_size):
+       
+        """
+        Parameters:
+        ----------
+        latent_size : int
+            latent dimension size of the autoencoder.
+        output_size : int
+            Output dimension for the data. Should equal input_dimension of AE.
+        kernel* : int, defualt=4
+            Convolutional filter size for layer *.
+        stride* : int, default=2
+            Stride length for convolutional filter at layer *.
+        """
+        self.latent_size = latent_size
+
+
+class LinEncoder(object):
+    def __init__(self, type, latent_size, output_size, num_layers, activation):
+       
+        """
+        Parameters:
+        ----------
+        latent_size : int
+            latent dimension size of the autoencoder.
+        output_size : int
+            Output dimension for the data. Should equal input_dimension of AE.
+        kernel* : int, defualt=4
+            Convolutional filter size for layer *.
+        stride* : int, default=2
+            Stride length for convolutional filter at layer *.
+        """
+        self.latent_size = latent_size
+        self.output_size = output_size
+
+        self.num_layers =  num_layers
+        self.activation = activation
+
+class LinDecoder(nn.Module):
+    def __init__(self, type, latent_size, output_size, num_layers, activation):
+       
+        """
+        Parameters:
+        ----------
+        latent_size : int
+            latent dimension size of the autoencoder.
+        output_size : int
+            Output dimension for the data. Should equal input_dimension of AE.
+        kernel* : int, defualt=4
+            Convolutional filter size for layer *.
+        stride* : int, default=2
+            Stride length for convolutional filter at layer *.
+        """
+        self.latent_size = latent_size
+        self.output_size = output_size
+
+        self.num_layers =  num_layers
+        self.activation = activation
+
+
 class AutoEncoder(object):
-    def __init__(self, type, mode, backend=None):
-
-        if (type != 'linear' and type != 'conv'):
-            raise Exception("type must be 'linear' or 'conv'")
-
-        self.type = type
+    def __init__(self, encoder, decoder, mode, backend=None):
+          """
+        Parameters:
+        ----------
+        encoder : LinEncoder or ConvEncoder
+            encoder options for the autoencoder.
+        decoder : LinDecoder or ConvDecoder
+            decoder options of the autoencoder.
+        mode : str
+            Decides backend. Either 'pytorch' or 'keras'.
+        backend : str
+            Required iff backend is 'keras'. Selects keras backend as 'tf' or 'th'.
+        """
+        if (decoder.latent_size != encoder.latent_size):
+            raise Exception("Encoder and decoder must have the same latent dimension.")
 
         if (mode != 'pytorch' and mode != 'keras'):
             raise Exception("mode must be 'pytorch' or 'keras'")
@@ -27,15 +97,26 @@ class AutoEncoder(object):
             
     def buildAutoEncoder(self):
         AE_model = None
-        if (self.type == 'linear'):
-            if (self.mode == 'pytorch'):
-                AE_model = buildPytorchVAE()
-            elif (self.mode == 'keras'):
-                AE_model = buildKerasVAE(self.backend)
-        elif (self.type == 'conv'):
-            if (self.mode == 'pytorch'):
-                AE_model = buildPytorchCVAE()
-            elif (self.mode == 'keras'):
-                AE_model = buildKerasCVAE(self.backend)
+        
+        if (self.mode == 'pytorch'):
+            AE_model = buildPytorchVAE()
+        elif (self.mode == 'keras'):
+            AE_model = buildKerasVAE(self.backend)
+        
+        if (self.mode == 'pytorch'):
+            AE_model = buildPytorchCVAE()
+        elif (self.mode == 'keras'):
+            AE_model = buildKerasCVAE(self.backend)
 
         return AE_model
+
+
+    def train(x_train, y_train=None):
+        if y_train == True:
+            pass
+        if mode is keras:
+            do keras
+
+
+    def predict(x_test, y_test=None):
+        pass
