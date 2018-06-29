95ba8f87d12a08b2c04a95e39c2bcc86cbd948ce
braceal <braceal@umich.edu>
2018-06-29 10:15:13 -0400

    Added implementation for encoder and decoder objects

--- Molecules/AutoEncoder.py
+++ Molecules/AutoEncoder.py
@@ -8,55 +8,83 @@ class ConvEncoder(object):
         ----------
         latent_size : int
             latent dimension size of the autoencoder.
-        output_size : int
-            Output dimension for the data. Should equal input_dimension of AE.
-        kernel* : int, defualt=4
-            Convolutional filter size for layer *.
-        stride* : int, default=2
-            Stride length for convolutional filter at layer *.
         """
+        if (latent_size < 0):
+            raise Exception("latent_size must be greater than 0!")
+
         self.latent_size = latent_size
 
-
-class LinEncoder(object):
-    def __init__(self, type, latent_size, output_size, num_layers, activation):
+class ConvDecoder(object):
+    def __init__(self, type, latent_size):
        
         """
         Parameters:
         ----------
         latent_size : int
             latent dimension size of the autoencoder.
-        output_size : int
-            Output dimension for the data. Should equal input_dimension of AE.
-        kernel* : int, defualt=4
-            Convolutional filter size for layer *.
-        stride* : int, default=2
-            Stride length for convolutional filter at layer *.
         """
+        if (latent_size < 0):
+            raise Exception("latent_size must be greater than 0!")
+
+        self.latent_size = latent_size
+
+class LinEncoder(object):
+    def __init__(self, latent_size, input_size, num_layers, activation):
+       
+        """
+        Parameters:
+        ----------
+        latent_size : int
+            latent dimension size of the autoencoder.
+        input_size : int
+            Input dimension for the data. Should equal the output dimension of the decoder.
+        num_layers : int
+            Number of linear dense layers to add to the encoder.
+        activation : str
+            Type of activation function EX) 'sigmoid', 'relu'
+        
+        """
+        if (latent_size < 0):
+            raise Exception("latent_size must be greater than 0!")
+        if (input_size < 0):
+            raise Exception("input_size must be greater than 0!")
+        if (num_layers < 0):
+            raise Exception("num_layers must be greater than 0!")
+
+        # TODO: Add exception handling for activation
+
         self.latent_size = latent_size
         self.output_size = output_size
-
         self.num_layers =  num_layers
         self.activation = activation
 
 class LinDecoder(nn.Module):
-    def __init__(self, type, latent_size, output_size, num_layers, activation):
+    def __init__(self, latent_size, output_size, num_layers, activation):
        
-        """
+         """
         Parameters:
         ----------
         latent_size : int
             latent dimension size of the autoencoder.
         output_size : int
-            Output dimension for the data. Should equal input_dimension of AE.
-        kernel* : int, defualt=4
-            Convolutional filter size for layer *.
-        stride* : int, default=2
-            Stride length for convolutional filter at layer *.
+            Output dimension for the data. Should equal the input dimension of the encoder.
+        num_layers : int
+            Number of linear dense layers to add to the decoder.
+        activation : str
+            Type of activation function EX) 'sigmoid', 'relu'
+        
         """
+        if (latent_size < 0):
+            raise Exception("latent_size must be greater than 0!")
+        if (output_size < 0):
+            raise Exception("output_size must be greater than 0!")
+        if (num_layers < 0):
+            raise Exception("num_layers must be greater than 0!")
+
+        # TODO: Add exception handling for activation
+
         self.latent_size = latent_size
         self.output_size = output_size
-
         self.num_layers =  num_layers
         self.activation = activation
 
@@ -73,7 +101,7 @@ class AutoEncoder(object):
         mode : str
             Decides backend. Either 'pytorch' or 'keras'.
         backend : str
-            Required iff backend is 'keras'. Selects keras backend as 'tf' or 'th'.
+            Required iff backend is 'keras'. Selects keras backend as 'tf' or 'th' or 'cntk'.
         """
         if (decoder.latent_size != encoder.latent_size):
             raise Exception("Encoder and decoder must have the same latent dimension.")
