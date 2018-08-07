2b91282ea9ee3f02882c5ad7289d36cfcda350cd
Alexander M. Brace <a05@mds-lnx16.ornl.gov>
2018-08-07 11:00:39 -0400

    Fixed tab issues in utils/Extract

--- molecules/utils/Extract/classification.py
+++ molecules/utils/Extract/classification.py
@@ -13,8 +13,8 @@ class ExtractClassification(object):
     def __init__(self, path_data_array="./input_data/native-contact/data/cont-mat.array",
                         n_traj=28, f_traj=10000, row=21, col=21):    
         """
-	    file_name = directory location of cont-mat.array
-	    """
+            file_name = directory location of cont-mat.array
+        """
         if (not os.path.exists(path_data_array)):
             raise Exception("Path " + str(path_data_array) + " does not exist!")
         if(n_traj < 0):
@@ -26,117 +26,114 @@ class ExtractClassification(object):
         if(col < 0):
             raise Exception("Invalid input: col must be greater than 0!")
 
-	    # define parameters;
-	    self.path_data_array = path_data_array
-	    self.n_traj = n_traj
-	    self.f_traj = f_traj
-	    # row and column dimension for each frame; 
-	    self.row = row
+        # define parameters;
+        self.path_data_array = path_data_array
+        self.n_traj = n_traj
+        self.f_traj = f_traj
+        # row and column dimension for each frame; 
+        self.row = row
         self.col = col
 
-    def build_array(self):	 
-	    # read line by line to save in array
-	    nf = self.n_traj * self.f_traj      
-	    q = self.row * self.col
-	    j_count = 0    
-	    k_count = 0   
-	    samples = (nf)
-	    row_num = (nf) * self.row
-	    column_num = (self.col)
-	    array_f_int = np.zeros(shape=(row_num, column_num))  
-	    with open(self.path_data_array) as infile:
-    	    for line in infile:    
-        	    array_f_string = line.split()  
-        	    array_f_array = np.array(list(array_f_string), dtype='|S4')  
-        	    array_f_float = array_f_array.astype(np.float)    
-        	    array_f_int[j_count] = array_f_float
-        	    if j_count == k_count:  
-            	    print("Frames read: ", (j_count/row))
-            	    k_count = k_count + 10000 * self.row      
-        	    j_count = j_count + 1
-        	    if j_count == (row_num):
-            	    break
+    def build_array(self):
+        # read line by line to save in array
+        nf = self.n_traj * self.f_traj      
+        q = self.row * self.col
+        j_count = 0    
+        k_count = 0   
+        samples = (nf)
+        row_num = (nf) * self.row
+        column_num = (self.col)
+        array_f_int = np.zeros(shape=(row_num, column_num))  
+        with open(self.path_data_array) as infile:
+        for line in infile:    
+            array_f_string = line.split()  
+            array_f_array = np.array(list(array_f_string), dtype='|S4')  
+            array_f_float = array_f_array.astype(np.float)    
+            array_f_int[j_count] = array_f_float
+            if j_count == k_count:  
+                print("Frames read: ", (j_count/row))
+                k_count = k_count + 10000 * self.row      
+            j_count = j_count + 1
+            if j_count == (row_num):
+                break
 
-	    print("Initial matrix array dimension:", np.shape(array_f_int))
-	    self.X = np.reshape(array_f_int, (samples, self.row, self.col))
-	    print("Final matrix array dimension:", np.shape(self.X))
-	    # select number of sample for data;
-	    self.X = self.X[0:1100000]
-	    # open contact map value for each frame;
-	    cont_mat = np.loadtxt(self.path_data_array)
-	    self.Y = cont_mat[0:len(self.X)]
-	    print("Input data shape:", np.shape(self.X))
-	    print("Input label shape:", np.shape(self.Y))
+        print("Initial matrix array dimension:", np.shape(array_f_int))
+        self.X = np.reshape(array_f_int, (samples, self.row, self.col))
+        print("Final matrix array dimension:", np.shape(self.X))
+        # select number of sample for data;
+        self.X = self.X[0:1100000]
+        # open contact map value for each frame;
+        cont_mat = np.loadtxt(self.path_data_array)
+        self.Y = cont_mat[0:len(self.X)]
+        print("Input data shape:", np.shape(self.X))
+        print("Input label shape:", np.shape(self.Y))
 
 
     def plot(self):
-	
-	    # plot histogram of npcontacts 
-	    [nhist, shist] = np.histogram(self.Y[ : ,1], 25);
-	    plt.semilogy(shist[1: ], nhist, 'r-');
-	    plt.show();
+       # plot histogram of npcontacts 
+       [nhist, shist] = np.histogram(self.Y[ : ,1], 25);
+       plt.semilogy(shist[1: ], nhist, 'r-');
+       plt.show();
 
 
     def calculate_states(self):
-	
-	    # calculate folded & unfolded state
-	    self.fold = list()
-	    self.unfold = list()
-	    # assign labels based on contact map value for each frame
-	    self.dat_1 = (self.Y[:,1:2])
-	    # assign label: fold=1 or unfold=0 (based on contact map value < or > 0.9)
-	    for i in range (0, len(self.dat_1)):
-    	    # folded state
-    	    if self.dat_1[i] > 0.9:
-        	    self.dat_1[i] = 1
-        	    fold.append(i)     
-    	    else:
-    		    # unfolded state
-        	    self.dat_1[i] = 0
-        	    unfold.append(i)   
-	    self.dat_1 = np.reshape(self.dat_1, (len(self.dat_1))) 
-	    print("Number of folded state:", len(self.fold)) 
-	    print("Number of unfolded state:", len(self.unfold))  
+        # calculate folded & unfolded state
+        self.fold = list()
+        self.unfold = list()
+        # assign labels based on contact map value for each frame
+        self.dat_1 = (self.Y[:,1:2])
+        # assign label: fold=1 or unfold=0 (based on contact map value < or > 0.9)
+        for i in range (0, len(self.dat_1)):
+            # folded state
+            if self.dat_1[i] > 0.9:
+                self.dat_1[i] = 1
+                fold.append(i)     
+           else:
+               # unfolded state
+               self.dat_1[i] = 0
+               unfold.append(i)   
+        self.dat_1 = np.reshape(self.dat_1, (len(self.dat_1))) 
+        print("Number of folded state:", len(self.fold)) 
+        print("Number of unfolded state:", len(self.unfold))  
 
     def shuffle(self):    
-	    # randomly choose folded & unfolded states with equal numbers
-	
-	    fold_cp = self.fold
-	    unfold_cp = self.unfold
-	    print("Choose %i number of folded or unfolded random labels" % len(self.unfold))    
-	    z = len(self.unfold)
-	    self.data = np.zeros((z*2, self.X.shape[1], self.X.shape[2]))
-	    self.label = np.zeros((z*2))
-	    self.index = np.zeros((z*2))
-	    print("Start random choices")
-	    # start random selections for folded configurations    
-	    for i in range (0, z): 
-    	    a = random.choice(fold_cp[:]) 
-    	    self.index[i] = a    
-    	    fold_cp.remove(a)   
-	    print("Finished choosing random choice for folded configurations")
-	    # start random selections for unfolded configurations    
-	    for i in range (z, z*2): 
-    	    b = random.choice(unfold_cp[:])
-    	    self.index[i] = b
-    	    unfold_cp.remove(b)
-	    print("Finished choosing random choice for unfolded configurations")
-	    # suffle elements      
-	    self.index.tolist()
-	    random.shuffle(self.index)    
-	    print("Shuffle finished")   
+        # randomly choose folded & unfolded states with equal numbers
+        fold_cp = self.fold
+        unfold_cp = self.unfold
+        print("Choose %i number of folded or unfolded random labels" % len(self.unfold))    
+        z = len(self.unfold)
+        self.data = np.zeros((z*2, self.X.shape[1], self.X.shape[2]))
+        self.label = np.zeros((z*2))
+        self.index = np.zeros((z*2))
+        print("Start random choices")
+        # start random selections for folded configurations    
+        for i in range (0, z): 
+            a = random.choice(fold_cp[:]) 
+            self.index[i] = a    
+            fold_cp.remove(a)   
+        print("Finished choosing random choice for folded configurations")
+        # start random selections for unfolded configurations    
+        for i in range (z, z*2): 
+            b = random.choice(unfold_cp[:])
+            self.index[i] = b
+            unfold_cp.remove(b)
+        print("Finished choosing random choice for unfolded configurations")
+        # suffle elements      
+        self.index.tolist()
+        random.shuffle(self.index)    
+        print("Shuffle finished")   
 
     def save(self,  data_format=None):
-	    if (data_format == None):
-	        print("Selecting default npy format type")
-	        data_format = 'npy'
+        if (data_format == None):
+            print("Selecting default npy format type")
+            data_format = 'npy'
         if ((data_format is not 'npy') and (data_format is not 'pkl')):
             raise Exception(str(data_format) + " is not a vaild data format.\nPlease chooose either 'npy' or 'pkl'.")
 
-	    file_name = "./input_data/cont-mat." + data_format
+        file_name = "./input_data/cont-mat." + data_format
 
 
-	    # store input data & lables in separate files    
+        # store input data & lables in separate files    
         w = 0
         for i in range (0, z*2):
             self.data[i,] = self.X[int(self.index[i,])]
@@ -149,18 +146,17 @@ class ExtractClassification(object):
         print("Data shape:", np.shape(self.data))
         print("Labels shape:", np.shape(self.label)
 
-	    if (data_format == 'npy'):
-	        label_file_name = "./input_data/label.npy"
-	        np.save(file_name, self.data)
-	        np.save(label_file_name, self.label)
-		
-	    elif (data_format == 'pkl'):
-	        result = (self.data, self.label)	
-	        # pickling extracted data
-	        # open file, pickle dumping & closing file    
- 	        file_object = open(file_name, 'wb')
-	        pickle.dump(result, file_object)
-	        file_object.close()
-	        # compress pickle file
-	        with open(file_name, 'rb') as f_in, gzip.open(file_name + ".gz", 'wb') as f_out:
-    	        shutil.copyfileobj(f_in, f_out)
+        if (data_format == 'npy'):
+            label_file_name = "./input_data/label.npy"
+            np.save(file_name, self.data)
+            np.save(label_file_name, self.label)
+        elif (data_format == 'pkl'):
+            result = (self.data, self.label)
+            # pickling extracted data
+            # open file, pickle dumping & closing file    
+            file_object = open(file_name, 'wb')
+            pickle.dump(result, file_object)
+            file_object.close()
+            # compress pickle file
+            with open(file_name, 'rb') as f_in, gzip.open(file_name + ".gz", 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
--- molecules/utils/Extract/distance_matrix.py
+++ molecules/utils/Extract/distance_matrix.py
@@ -16,13 +16,17 @@ from MDAnalysis.analysis import distances;
 class ExtractDistanceMatrix(object):
     
     def __init__(self, structure_path=None, trajectory_path=None, n=1, f = 10000):
-	
-	    """
-        structure_path = directory path containing the desired .pdb file (include pdb file in path).
-        trajectory_path = directory path containing the desired .xtc, .dcd trajectory files (do not include trajectory files).
-                          Trajectory files should be labeled trajectory-i.xtc where i is the number of the file.
-        n = number of trajectories (trajectory files)
-        f = number of frames per trajectory.
+ 
+        """
+        structure_path : str
+            directory path containing the desired .pdb file (include pdb file in path).
+        trajectory_path : str
+            directory path containing the desired .xtc, .dcd trajectory files (do not include trajectory files).
+            Trajectory files should be labeled trajectory-i.xtc where i is the number of the file.
+        n : int
+            number of trajectories (trajectory files)
+        f : int
+            number of frames per trajectory.
         """
 
         if(structure_path == None or trajectory_path == None):
@@ -38,34 +42,34 @@ class ExtractDistanceMatrix(object):
         # TODO: build funciton to parse n and f from structure_path and trajectory_path
         self.structure_path = structure_path
         self.trajectory_path = trajectory_path
- 	    # number of trajectory files
+        # number of trajectory files
         self.n = n
 
         # number of frames per trajectories;
         self.f = f
 
-	    # defining holder for distance matrix;
-	    self.ca_tot = [];
-	
+        # defining holder for distance matrix;
+        self.ca_tot = [];
+
     def calculate_distance_matrix(self):
-	    # calculate contact map over frames;
-	    for i in range(1, (self.n+1)):    
-    	    # specify path of structure & trajectory files;    
-    	    u0 =mdanal.Universe(self.structure_path, self.trajectory_path + '/trajectory-%i.xtc' % i)
-    	    # crude definition of salt bridges as contacts between CA atoms;
-    	    CA1 = "(name CA and resid 55)";
-    	    CA2 = "(name CA and resid 154)";
-    	    CA10 = u0.select_atoms(CA1);
-    	    CA20 = u0.select_atoms(CA2);
-    	    #CA0 = u0.select_atoms(CA);
-    	    # print progress;
-    	    print("Read user defined atoms for frames:", i * self.f);
-    	    # calculate contact map over all frames; 
-    	    for ts in u0.trajectory[0:self.f]:
-        	# calculating and saving native contact dat files per frame;     
-        	# set up analysis of native contacts ("salt bridges"); salt bridges have a distance <8 Angstrom;
-        	ca_dist = distances.distance_array(CA10.coordinates(), CA20.coordinates()); 
-        	self.ca_tot = np.append(self.ca_tot,ca_dist);
+        # calculate contact map over frames;
+        for i in range(1, (self.n+1)):    
+            # specify path of structure & trajectory files;    
+            u0 =mdanal.Universe(self.structure_path, self.trajectory_path + '/trajectory-%i.xtc' % i)
+            # crude definition of salt bridges as contacts between CA atoms;
+            CA1 = "(name CA and resid 55)";
+            CA2 = "(name CA and resid 154)";
+            CA10 = u0.select_atoms(CA1);
+            CA20 = u0.select_atoms(CA2);
+            #CA0 = u0.select_atoms(CA);
+            # print progress;
+            print("Read user defined atoms for frames:", i * self.f);
+            # calculate contact map over all frames; 
+            for ts in u0.trajectory[0:self.f]:
+                # calculating and saving native contact dat files per frame;     
+                # set up analysis of native contacts ("salt bridges"); salt bridges have a distance <8 Angstrom;
+               ca_dist = distances.distance_array(CA10.coordinates(), CA20.coordinates()); 
+               self.ca_tot = np.append(self.ca_tot,ca_dist);
 
     def build_directories(self):
         """
@@ -85,15 +89,15 @@ class ExtractDistanceMatrix(object):
         print("Completed directories creation or if already exist - then checked")
 
     def save(self)
-	    #reshaping distance matrix & saving;
-	    self.ca_tot = np.reshape((self.ca_tot), (len(self.ca_tot), 1)); 
-	    print np.shape(self.ca_tot);
-	    np.save("./input_data/native-contact/data/dist-mat.npy", self.ca_tot);
+        #reshaping distance matrix & saving;
+        self.ca_tot = np.reshape((self.ca_tot), (len(self.ca_tot), 1)); 
+        print np.shape(self.ca_tot);
+        np.save("./input_data/native-contact/data/dist-mat.npy", self.ca_tot);
 
 
     def plot(self):
-    	# plot histogram of native contacts;    
-	    dat_check = np.load('./input_data/native-contact/data/dist-mat.npy');
-	    [nhist, shist] = np.histogram(dat_check[ : ,0], 25);
-	    plt.semilogy(shist[1: ], nhist, 'r-');
-	    plt.show();
+        # plot histogram of native contacts;    
+        dat_check = np.load('./input_data/native-contact/data/dist-mat.npy');
+        [nhist, shist] = np.histogram(dat_check[ : ,0], 25);
+        plt.semilogy(shist[1: ], nhist, 'r-');
+        plt.show();
